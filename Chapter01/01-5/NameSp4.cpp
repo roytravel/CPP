e73df911daf8f32281d24e73365a9c5d6b7a020a
roytravel <roytravel97@gmail.com>
2020-02-22 23:36:30 +0900

    Add(01-5): check how to use nested namespace

---
+++ Chapter01/01-5/NameSp4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+namespace Parent
+{
+	int num=2;
+	
+	namespace SubOne
+	{
+		int num=3;
+	}
+	
+	namespace SubTwo
+	{
+		int num=4;
+	}
+}
+
+int main(void)
+{
+	cout << Parent::num << endl;
+	cout << Parent::SubOne::num << endl;
+	cout << Parent::SubTwo::num << endl;
+	return 0;
+}
