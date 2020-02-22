0c857d39d6e43ea7011575f040420d3eb07c21b3
roytravel <roytravel97@gmail.com>
2020-02-22 17:28:59 +0900

    Add(01-3): implement defaultvalue3.cpp using function overloading

---
+++ Problem/01-3/Problem01-31.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int BoxVolume(int length, int width, int height)
+{
+	return length * width * height;
+}
+
+int BoxVolume (int length, int width)
+{
+	return length, width * 1;
+}
+
+int BoxVolume (int length)
+{
+	return length * 1 * 1;
+}
+
+int main(void)
+{
+	cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << endl;
+	cout << "[3, 5, D] : " << BoxVolume(5, 5) << endl;
+	cout << "[7, D, D] : " << BoxVolume(7) << endl;
+	return 0;
+}
