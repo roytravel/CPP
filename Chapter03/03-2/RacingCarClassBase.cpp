9b9450d93744efb0ada91e907baf3bcb5aeaaef3
roytravel <roytravel97@gmail.com>
2020-02-24 20:29:10 +0900

    Add(03-2): implement car drive example using the concept of class

---
+++ Chapter03/03-2/RacingCarClassBase.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+namespace CAR_CONST
+{
+    enum
+    {
+        ID_LEN=20, MAX_SPD=200, FUEL_STEP=2,
+        ACC_STEP=10, BRK_STEP=10
+    };
+}
+
+
+class Car
+{
+private:
+    char gamerID[CAR_CONST::ID_LEN];
+    int fuelGauge;
+    int curSpeed;
+
+public:
+    void InitMembers(char * ID, int fuel);
+    void ShowCarState();
+    void Accel();
+    void Break();
+};
+
+
+void Car::InitMembers(char * ID, int fuel)
+{
+    strcpy(gamerID, ID);
+    fuelGauge = fuel;
+    curSpeed = 0;
+}
+
+
+void Car::ShowCarState()
+{
+    cout << "소유자ID : " << gamerID << endl;
+    cout << "연료량: " << fuelGauge << "%" << endl;
+    cout << "현재속도: " << curSpeed << "km/s" << endl << endl;
+}
+
+
+void Car::Accel()
+{
+    if (fuelGauge <= 0)
+        return ;
+
+    else
+        fuelGauge = fuelGauge - CAR_CONST::FUEL_STEP;
+    
+    if ((curSpeed + CAR_CONST::ACC_STEP) >= CAR_CONST::MAX_SPD)
+    {
+        curSpeed = CAR_CONST::MAX_SPD;
+        return;
+    }
+
+    curSpeed = curSpeed + CAR_CONST::ACC_STEP;
+}
+
+
+void Car::Break()
+{
+    if(curSpeed < CAR_CONST::BRK_STEP)
+    {
+        curSpeed = 0;
+        return;
+    }
+
+    curSpeed = curSpeed - CAR_CONST::BRK_STEP;
+}
+
+
+int main(void)
+{
+    Car eol99;
+    eol99.InitMembers("eol99", 100);
+    eol99.Accel();
+    eol99.Accel();
+    eol99.Accel();
+    eol99.ShowCarState();
+    eol99.Break();
+    eol99.ShowCarState();
+
+    return 0;
+}
